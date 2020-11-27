dca4d1e3dc2b3721e51b3a827817eaf0fac6108a
Stelios <svtozio@csd.auth.gr>
2020-11-27 23:05:45 +0200

    3st tried.

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Game
 {
-    public Game ()
-    {
+    private int numberOfRounds;
 
+    public Game (int numberOfRounds)
+    {
+        this.numberOfRounds = numberOfRounds;
+    }
+
+    public int hasRounds (int numberOfRounds)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("How many rounds do you want to play?\n");
+        this.numberOfRounds = scanner.nextInt();
+        //scanner.nextLine();
+
+        return this.numberOfRounds;
     }
 }
---
+++ src/Round.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Round
+{
+    private int numberOfQuestions;
+    private int points;
+    private String kind;
+
+    public Round (int numberOfQuestions, String kind, int points)
+    {
+        this.numberOfQuestions = numberOfQuestions;
+        this.kind = kind;
+        this.points = points;
+    }
+
+    public int hasQuestions (int numberOfQuestions)
+    {
+        this.numberOfQuestions = 8;
+        System.out.printf("Number of Questions iof this rounds are: %d\n", this.numberOfQuestions);
+
+        return this.numberOfQuestions;
+    }
+
+    public void isKind ()
+    {
+        System.out.printf("%s\n", this.kind);
+    }
+
+    public void showPoint ()
+    {
+        System.out.printf("%d\n", this.points);
+    }
+}
